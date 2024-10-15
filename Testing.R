97f8418853e643441a0e5eae371ac6698cb00890
Divya Priyanka. T <156469276+Divya090597@users.noreply.github.com>
2024-10-15 17:41:52 -0400

    Update Testing.R

--- Testing.R
+++ Testing.R
@@ -4,3 +4,7 @@ file_path = "~/data/Kidney/ScGate_project/Scgate_Myeloidcell_example_genelist_20
 
 my_model <- load_mac_model(file_path)
 
+# Function_2
+Macrophage = "https://raw.githubusercontent.com/Divya090597/Building_Automated_Cell_Annotation_Package/refs/heads/main/Scgate_Myeloidcell_example_genelist_2024_10_04.tsv.txt"
+
+model = load_model(Macrophage)
